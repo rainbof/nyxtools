6e72686c70e9319b06f9a3169ca44a6e3314b646
Marek Sebera <marek.sebera@gmail.com>
2013-04-14 17:40:40 +0200

    Instance is taking Context for Preferences purpose

--- src/com/rainbof/nyxtools/NyxTools.java
+++ src/com/rainbof/nyxtools/NyxTools.java
@@ -1,20 +1,25 @@
 package com.rainbof.nyxtools;
 
+import android.content.Context;
+
 public class NyxTools {
 
 	private volatile static NyxTools mInstance;
+	private NyxToolsPersistence persistence;
 
-	public static NyxTools getInstance() {
+	public static NyxTools getInstance(Context _context) {
 		if (mInstance == null) {
 			synchronized (NyxTools.class) {
-				mInstance = new NyxTools();
+				mInstance = new NyxTools(_context);
 			}
 		}
 		return mInstance;
 	}
 	
-	protected NyxTools(){
-		
+	protected NyxTools(Context _context){
+		if(_context.getApplicationContext() != null)
+			_context = _context.getApplicationContext();
+		persistence = new NyxToolsPersistence(_context);
 	}
 
 }
